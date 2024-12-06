8cd5516685156eee055a9a0cb8c18e9a873b55e9
cxhacks_17 <chrisminepro@gmail.com>
2024-12-05 21:34:06 -0600

    Agregar archivos de proyecto.

---
+++ 21300675_CRUD_7H1/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _21300675_CRUD_7H1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ 21300675_CRUD_7H1/Modelos/Asignatura.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21300675_CRUD_7H1.Modelos
+{
+    internal class Asignatura
+    {
+    }
+}
